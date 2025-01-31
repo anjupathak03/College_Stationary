1199c0e34ee1370ff7670528e4542b5178fb7131
Anju <anjupathak9810@gmail.com>
2025-01-31 23:56:30 +0530

    changes for vercel

--- Frontend/src/components/Freeservices.jsx
+++ Frontend/src/components/Freeservices.jsx
@@ -9,7 +9,7 @@ function Freeservices() {
   useEffect(() => {
     const getBook = async()=>{
       try{
-          const res = await axios.get("http://localhost:4001/book");
+          const res = await axios.get("https://college-stationary.onrender.com/book");
           
           const data = res.data.filter((data) => data.category === "free");
           console.log(data);
